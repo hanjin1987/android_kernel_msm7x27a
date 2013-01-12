8dbd16cd3b214de3676e196e62b9e23d868ae765
Daz Jones <yuki@thebronasium.com>
2013-01-12 21:19:39 +0000

    update build

--- include/linux/netfilter_ipv4/ipt_ttl.h
+++ include/linux/netfilter_ipv4/ipt_ttl.h
@@ -1,5 +1,5 @@
 /* IP tables module for matching the value of the TTL
-* (C) 2000 by Harald Welte <laforge@gnumonks.org> */
+ * (C) 2000 by Harald Welte <laforge@gnumonks.org> */
 
 #ifndef _IPT_TTL_H
 #define _IPT_TTL_H
@@ -7,16 +7,16 @@
 #include <linux/types.h>
 
 enum {
-IPT_TTL_EQ = 0,	/* equals */
-IPT_TTL_NE,	/* not equals */
-IPT_TTL_LT,	/* less than */
-IPT_TTL_GT,	/* greater than */
+	IPT_TTL_EQ = 0,		/* equals */
+	IPT_TTL_NE,		/* not equals */
+	IPT_TTL_LT,		/* less than */
+	IPT_TTL_GT,		/* greater than */
 };
 
 
 struct ipt_ttl_info {
-__u8	mode;
-__u8	ttl;
+	__u8	mode;
+	__u8	ttl;
 };
 
 
